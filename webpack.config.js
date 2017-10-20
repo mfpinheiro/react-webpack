9c82efb761ef73faf5c3d9d289f3d074ccd9bea8
mfpinheiro <mateusfpinheiro@gmail.com>
2017-10-19 23:33:22 -0200

    Add html-webpack-plugin to handle html content

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 // Using node native path module
 const path = require('path');
-
+// plugin to handle html files
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // Constants to paths
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
   JS: path.resolve(__dirname, 'src/js'),
 };
 
@@ -14,4 +16,12 @@ module.exports = {
     path: paths.DIST,
     filename: 'app.bundle.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+  ],
+  // devServer: {
+  //   contentBase: paths.SRC,
+  // },
 };
