06e4e8e60f1757544667655a8137386e1f51834d
mfpinheiro <mateusfpinheiro@gmail.com>
2017-10-19 23:13:46 -0200

    First Commit

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by mfpinheiro on 19/10/17.
+ */
+console.log('Howdy');
+
+// Command to generate the bundle file
+// ./node_modules/webpack/bin/webpack.js ./src/js/app.js --output-filename ./dist/app.bundle.js
