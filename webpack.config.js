bf7a92f9ae9a6b5c6276b79b9e5241eef286dd54
mfpinheiro <mateusfpinheiro@gmail.com>
2017-10-19 23:20:36 -0200

    Add first webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// Using node native path module
+const path = require('path');
+
+// Constants to paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+};
